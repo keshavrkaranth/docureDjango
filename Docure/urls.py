75ba1b79842222850c11d21644714f9e13f1786d
Keshavkaranth <keshavarkarantha@gmail.com>
2021-06-03 23:24:14 +0530

    rebase-3

--- Docure/urls.py
+++ Docure/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static, serve
-from django.conf.urls import url
+from django.conf.urls.static import static
+
 
 
 urlpatterns = [
@@ -25,9 +25,6 @@ urlpatterns = [
     path('', include('docureapp.urls')),
     path('products/', include('store.urls')),
     path('cart/', include('cart.urls')),
-    url(r'^media/(?P<path>.*)$', serve,
-        {'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,
-        {'document_root': settings.STATIC_ROOT}),
+
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
