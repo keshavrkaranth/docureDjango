53225730d6679c4f248bbbbc050fc46245f89f6c
Keshavkaranth <keshav@ohlocal.in>
2021-08-18 15:00:06 +0530

    edit

--- Docure/settings.py
+++ Docure/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure--+2azm@xdxf4(p)&$ni2liist*)9^re+s)xv)$x2cw(42qb2b0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["ayurhealth.herokuapp.com", '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
