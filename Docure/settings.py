a9a3b9521f91828d452ee677629fb2fd1502abdf
Keshavkaranth <keshav@ohlocal.in>
2021-08-18 15:14:42 +0530

    refactored Debug

--- Docure/settings.py
+++ Docure/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--+2azm@xdxf4(p)&$ni2liist*)9^re+s)xv)$x2cw(42qb2b0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
